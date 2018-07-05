5b4e39235fe0147faaa74f7847d2e93248709add
ilmirgogorev <ilmirgogorev@yandex.ru>
2018-07-06 01:27:49 +0300

    Eye with blur

--- Eye.py
+++ Eye.py
@@ -18,11 +18,16 @@ cv2.createTrackbar('v1', 'settings', 0, 255, nothing)
 cv2.createTrackbar('h2', 'settings', 180, 180, nothing)
 cv2.createTrackbar('s2', 'settings', 255, 255, nothing)
 cv2.createTrackbar('v2', 'settings', 255, 255, nothing)
+cv2.createTrackbar('g', 'settings', 5, 15, nothing)
 
 while 1:
 
     hsv = cv2.cvtColor(in_img, cv2.COLOR_BGR2HSV)
-
+    g = cv2.getTrackbarPos('g', 'settings')
+    d = divmod(g, 2)
+    if d[1] == 0:
+        g = g - 1
+    hsv = cv2.GaussianBlur(hsv, (g, g), 2)
     h1 = cv2.getTrackbarPos('h1', 'settings')
     s1 = cv2.getTrackbarPos('s1', 'settings')
     v1 = cv2.getTrackbarPos('v1', 'settings')
@@ -34,6 +39,7 @@ while 1:
     h_max = (h2, s2, v2)
 
     mask = cv2.inRange(hsv, h_min, h_max)
+    mask = cv2.dilate(mask, (5, 5), iterations=1)
     obj = cv2.bitwise_and(in_img, in_img, mask=mask)
 
     cv2.imshow("Original image", in_img)
